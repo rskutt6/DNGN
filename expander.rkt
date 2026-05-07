5dfcc6fd44b057937a3e0579fa3ea5435c7404cc
MaanasaYepuru <134438911+MaanasaYepuru@users.noreply.github.com>
2026-05-06 22:15:53 -0500

    Update expander.rkt

--- expander.rkt
+++ expander.rkt
@@ -8,16 +8,8 @@
 
 (provide (rename-out [dungeon-module-begin #%module-begin]))
 (provide (matching-identifiers-out
-          #rx"^(program|room|room-body|room-element|desc|item|item-body|item-field|type|monster|exit|power)$"
+          #rx"^(program|room|room-body|room-element|desc|item|item-body|item-field|type|monster|exit|power|key)$"
           (all-defined-out)))
-
-;; runtime data
-
-;(struct game (rooms) #:transparent)
-;(struct room-v (name desc items monsters exits power) #:transparent)
-;(struct item-v (name desc type) #:transparent)
-;(struct monster-v (name hp) #:transparent)
-;(struct exit-v (direction destination) #:transparent)
 
 ;; small wrapper values used while assembling room and items.
 
@@ -27,81 +19,57 @@
 (struct exit-node (value) #:transparent)
 (struct power-node (value) #:transparent)
 (struct type-node (value) #:transparent)
+(struct key-node (value) #:transparent)
 
 ;; HELPER: remove surrounding quotes from strings
 
 (define (unquote s)
   (if (and (string? s)
            (>= (string-length s) 2)
-           (string=? (substring s 0 1) "\"") ; first char is "
-           (string=? (substring s (sub1 (string-length s))) "\"")) ; last char is "
-      ;; strip off the first and last character
+           (string=? (substring s 0 1) "\"")
+           (string=? (substring s (sub1 (string-length s))) "\""))
       (substring s 1 (sub1 (string-length s)))
-      ;; otherwise leave unchanged
       s))
 
 ;; BUILD AN ITEM FROM ITS FIELDS
 
-;; function walks over fields, picks out the desc and type, and produces one final item-v stuct
 (define (build-item name fields)
-  ;; start with not found yet values
   (define desc-text #f)
   (define item-type #f)
 
-  ;; loop over each field inside the item
   (for ([f fields])
     (match f
-      ;; if field = desc node, extract text, remove quotes and store it
       [(desc-node txt)
        (set! desc-text (unquote txt))]
-
-      ;; if the field is a type node, store the type
       [(type-node t)
        (set! item-type t)]
-
-      ;; any other field is invalid
       [_ (error 'build-item "invalid item field: ~a" f)]))
 
-  ;; return final item value
   (item-v name desc-text item-type))
 
 ;; BUILD A ROOM FROM ITS ELEMENTS
 
 (define (build-room name elements)
-  ;; fields we expect to accumulate
   (define desc-text #f)
   (define items '())
   (define monsters '())
   (define exits '())
   (define room-power #f)
 
-  ;; walk through each element inside the room
   (for ([el elements])
     (match el
-      ;; Room desc
       [(desc-node txt)
        (set! desc-text (unquote txt))]
-
-      ;; Item element
       [(item-node i)
        (set! items (cons i items))]
-
-      ;; Monster element
       [(monster-node m)
        (set! monsters (cons m monsters))]
-
-      ;; Exit element
       [(exit-node e)
        (set! exits (cons e exits))]
-
-      ;; Power element
       [(power-node p)
        (set! room-power p)]
-
-      ;; anything else not allowed
       [_ (error 'build-room "invalid room element: ~a" el)]))
 
-  ;; reverse restores the source order (because list was built with cons)
   (room-v name
           desc-text
           (reverse items)
@@ -109,10 +77,9 @@
           (reverse exits)
           room-power))
 
-;; PARSE_TREE EXPANDERS
+;; PARSE TREE EXPANDERS
 
 ;; ---room---
-;; each room will expand into a definition
 
 (define-macro (room NAME BODY)
   (with-pattern ([ROOM-ID (prefix-id "room-" #'NAME #:source #'NAME)])
@@ -147,6 +114,11 @@
 (define-macro (type NAME)
   #'(type-node NAME))
 
+;; ---key---
+
+(define-macro (key NAME)
+  #'NAME)
+
 ;; ---monster---
 
 (define-macro (monster NAME HP)
@@ -154,8 +126,11 @@
 
 ;; ---exit---
 
-(define-macro (exit DIR DEST)
-  #'(exit-node (exit-v DIR DEST)))
+ (define-macro (exit DIR DEST . REST)
+  (with-pattern ([KEY (if (null? (syntax->list #'REST))
+                          #'#f
+                          (car (syntax->list #'REST)))])
+    #'(exit-node (exit-v DIR DEST KEY))))
 
 ;; ---power---
 
@@ -163,7 +138,6 @@
   #'(power-node N))
 
 ;; -----dungeon-module-begin-----
-;; macro for the whole program
 
 (define-macro (dungeon-module-begin (program ROOM ...))
   (with-pattern
