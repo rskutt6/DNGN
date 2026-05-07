d3217766a8ac396e7d3fa670e9dc22bbfa48948d
MaanasaYepuru <134438911+MaanasaYepuru@users.noreply.github.com>
2026-05-06 22:17:16 -0500

    Update parser.rkt

--- parser.rkt
+++ parser.rkt
@@ -1,36 +1,22 @@
 #lang brag
-
 program : room*
-
 room : /LPAREN /"room" ID room-body /RPAREN
-
 room-body : room-element*
           
-
 room-element : desc
              | item
              | monster
              | exit
              | power
-
 desc : /LPAREN /"desc" STRING /RPAREN
-
 item : /LPAREN /"item" ID item-body /RPAREN
-
 item-body : item-field*
           
-
 item-field : desc
            | type
-
 type : /LPAREN /"type" ID /RPAREN
-
 monster : /LPAREN /"monster" ID NUMBER /RPAREN
-
-exit : /LPAREN /"exit" ID ID exit-extra? /RPAREN
-exit-extra : key
+exit : /LPAREN /"exit" ID ID key? /RPAREN
 key : /LPAREN /"key" ID /RPAREN
-
 power : /LPAREN /"power" NUMBER /RPAREN
-
 
