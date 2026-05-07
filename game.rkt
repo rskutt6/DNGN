1f08fbcccbddbdf4e2b60852810b10d9cd07c4eb
MaanasaYepuru <134438911+MaanasaYepuru@users.noreply.github.com>
2026-05-06 19:56:27 -0500

    Update game.rkt

--- game.rkt
+++ game.rkt
@@ -1,41 +1,41 @@
 #lang racket
 (require racket/string
          racket/gui/base)
- 
+
 ; -----------------------------------------------
 ; STRUCTS
 ; define the data shapes for game world
 ; -----------------------------------------------
- 
+
 (struct game (rooms) #:transparent)
 (struct room-v (name desc items monsters exits power) #:transparent)
 (struct item-v (name desc type) #:transparent)
 (struct monster-v (name hp) #:transparent)
 (struct exit-v (direction destination key) #:transparent)
- 
+
 (define (get-room rooms name)
   (cdr (assoc name rooms)))
- 
+
 ;; -----------------------------------------------
 ;; GRAPHICS
 ;; one image window, smaller centered images
 ;; -----------------------------------------------
- 
+
 (define image-frame #f)
 (define image-canvas #f)
 (define current-bitmap #f)
- 
+
 (define WINDOW-WIDTH 700)
 (define WINDOW-HEIGHT 500)
 (define IMAGE-SCALE 0.5) ; change to 0.5 smaller, 0.8 bigger
- 
+
 (define (show-room-image room-name)
   (define path (string-append "images/" room-name ".png"))
- 
+
   (if (file-exists? path)
       (begin
         (set! current-bitmap (make-object bitmap% path))
- 
+
         (if image-frame
             (send image-canvas refresh-now)
             (begin
@@ -44,7 +44,7 @@
                          [label "Dungeon View"]
                          [width WINDOW-WIDTH]
                          [height WINDOW-HEIGHT]))
- 
+
               (set! image-canvas
                     (new canvas%
                          [parent image-frame]
@@ -55,27 +55,27 @@
                             (when current-bitmap
                               (define img-w (send current-bitmap get-width))
                               (define img-h (send current-bitmap get-height))
- 
+
                               (define draw-w (* img-w IMAGE-SCALE))
                               (define draw-h (* img-h IMAGE-SCALE))
- 
+
                               (define x (/ (- WINDOW-WIDTH draw-w) 2))
                               (define y (/ (- WINDOW-HEIGHT draw-h) 2))
- 
+
                               (send dc set-scale IMAGE-SCALE IMAGE-SCALE)
                               (send dc draw-bitmap current-bitmap
                                     (/ x IMAGE-SCALE)
                                     (/ y IMAGE-SCALE))
                               (send dc set-scale 1 1)))]))
- 
+
               (send image-frame show #t))))
       (displayln (string-append "Missing image: " path))))
- 
+
 ; -----------------------------------------------
 ; PLAY
 ; entry point called by the expander
 ; -----------------------------------------------
- 
+
 (define (play world)
   (show-room-image "map")
   (displayln "==========================================")
@@ -83,20 +83,20 @@
   (displayln "==========================================")
   (displayln "Study the map. Press ENTER when you are ready to begin.")
   (read-line)
- 
+
   (define rooms (game-rooms world))
   (define start-name (car (car rooms)))
   (game-loop rooms start-name 10 '()))
- 
+
 ; -----------------------------------------------
 ; GAME LOOP
 ; called when player enters new room or after an action
 ; -----------------------------------------------
- 
+
 (define (game-loop rooms current player-power inventory)
   (define r (get-room rooms current))
   (show-room-image current)
- 
+
   ; ---- WIN CONDITION ----
   (when (null? (room-v-exits r))
     (displayln "==========================================")
@@ -106,7 +106,7 @@
     (printf "    You escaped with a power of ~a!\n" player-power)
     (displayln "==========================================")
     (exit))
- 
+
   ; ---- PRINT ROOM INFO ----
   (displayln "==========================================")
   (printf "  ~a~aPower: ~a\n"
@@ -115,18 +115,17 @@
           player-power)
   (displayln "==========================================")
   (printf "  ~a\n\n" (room-v-desc r))
- 
+
   (printf "  Exits: ~a\n"
           (string-join
-           (map (lambda (e)
-                  (symbol->string (exit-v-direction e)))
+           (map (lambda (e) (exit-v-direction e))
                 (room-v-exits r))
            " | "))
- 
+
   (when (not (null? (room-v-items r)))
     (printf "  Items:    ~a\n"
             (string-join (map item-v-name (room-v-items r)) ", ")))
- 
+
   (when (not (null? (room-v-monsters r)))
     (printf "  Monsters: ~a\n"
             (string-join
@@ -136,14 +135,14 @@
                             (monster-v-hp m)))
                   (room-v-monsters r))
              ", ")))
- 
+
   (printf "  Inventory: ~a\n"
           (if (null? inventory)
               "empty"
               (string-join (map item-v-name inventory) ", ")))
- 
+
   (displayln "==========================================")
- 
+
   ; ---- ITEM PICKUP PHASE ----
   (define-values (power-after-items new-inventory)
     (cond
@@ -153,7 +152,7 @@
        (displayln "Type 'take <item>' or 'continue'")
        (display "> ")
        (define item-input (read-line))
- 
+
        (if (and (>= (string-length item-input) 5)
                 (equal? (substring item-input 0 5) "take "))
            (let* ([item-name (substring item-input 5)]
@@ -170,7 +169,7 @@
                   (cons found-item inventory))
                  (values player-power inventory)))
            (values player-power inventory))]))
- 
+
   ; ---- COMBAT PHASE ----
   (define power-after-combat
     (if (null? (room-v-monsters r))
@@ -182,28 +181,27 @@
           (display "fight or run? > ")
           (define choice (read-line))
           (when (equal? choice "quit") (displayln "Goodbye!") (exit))
- 
+
           (cond
             ; --- RUN ---
             [(equal? choice "run")
              (displayln "You run! Which way?")
              (for ([e (room-v-exits r)])
-               (printf "Exit: ~a\n"
-                       (exit-v-direction e)))
+               (printf "Exit: ~a\n" (exit-v-direction e)))
              (display "> ")
              (define run-dir (read-line))
- 
+
              (define run-dest #f)
              (for ([e (room-v-exits r)])
                (when (equal? (exit-v-direction e) run-dir)
                  (set! run-dest (exit-v-destination e))))
- 
+
              (if run-dest
                  (game-loop rooms run-dest power-after-items new-inventory)
                  (begin
                    (displayln "Can't go that way!")
                    (game-loop rooms current power-after-items new-inventory)))]
- 
+
             ; --- FIGHT ---
             [(equal? choice "fight")
              (if (>= power-after-items (monster-v-hp m))
@@ -213,28 +211,27 @@
                  (begin
                    (displayln "You are too weak... YOU DIED!")
                    (exit)))]
- 
+
             ; --- INVALID ---
             [else
              (displayln "Type 'fight' or 'run'.")
              (game-loop rooms current power-after-items new-inventory)]))))
- 
+
   ; ---- MOVEMENT PHASE (WITH LOCKED DOORS) ----
   (displayln "Where do you go?")
   (display "> ")
   (define input (read-line))
- 
+
   (define next-exit
     (findf (lambda (e)
-             (equal? (symbol->string (exit-v-direction e)) input))
+             (equal? (exit-v-direction e) input))
            (room-v-exits r)))
- 
+
   (if next-exit
       (let ([required-key (exit-v-key next-exit)])
         (if (or (not required-key)
                 (findf (lambda (i)
-                         (equal? (item-v-name i)
-                                 (symbol->string required-key)))
+                         (equal? (item-v-name i) required-key))
                        new-inventory))
             (game-loop rooms
                        (exit-v-destination next-exit)
@@ -246,6 +243,5 @@
       (begin
         (displayln "Can't go that way.")
         (game-loop rooms current power-after-combat new-inventory))))
- 
+
 (provide (all-defined-out))
- 
